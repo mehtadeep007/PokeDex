bde8fa27268a713971e8fda081ba65aa410de4e9
mehtadeep007 <mehtadeep2013@gmail.com>
2020-07-14 15:27:49 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { getPokemon, getAllPokemon } from './services/pokemon';
 import './App.css';
+import Navbar from './components/Navbar';
+import Card from './components/Card/Card';
 
 function App() {
+
+  const [pokemonData, setPokemonData] = useState([])
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialURL)
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord
+    }))
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        {loading ? <h1 style={{ textAlign: 'center' }}>Loading...</h1> : (
+          <>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+            <div className="grid-container">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            Pokemon API
+        </div>
+    );
+}
+
+export default Navbar;
